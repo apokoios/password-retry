f96b0485bcf25d3eb599ae4db6691a7979e5b250
Wan-Jhen <apokoios@gmail.com>
2022-12-13 23:27:39 +0800

    basic version done

---
+++ password-retry.py
@@ -0,0 +1,10 @@
+password = 'a123456'
+while True: 
+	pwd = input('請設定密碼: ')
+	if pwd == 'a123456' :
+    print('登入成功')
+		break
+	elif pwd == 'a123456' :
+		print('密碼錯誤!還有二次機會') 
+	elif pwd == 'a123456' :
+        print('密碼錯誤!還有一次機會') 
